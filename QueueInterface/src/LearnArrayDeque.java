0e52b326e9e9863d0ae4f9f8d60ee3836b00bfb7
Prince Singh <pprincesinghp@live.in>
2023-12-12 19:20:57 +0530

    added ArrayDeque

--- QueueInterface/src/LearnArrayDeque.java
+++ QueueInterface/src/LearnArrayDeque.java
@@ -1,5 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Vector;
+
 public class LearnArrayDeque {
     public static void main(String[] args) {
+        ArrayDeque<Integer> adq = new ArrayDeque<>();
+        adq.offer(30);
+        adq.offerFirst(15); //will add element at first
+        adq.offerLast(45);
+        adq.offer(26);
+        System.out.println(adq);
 
+        System.out.println(adq.peek());
+        System.out.println(adq.peekFirst());
+        System.out.println(adq.peekLast());
+
+        System.out.println(adq.poll());
+        System.out.println("Poll() "+adq);
+
+        System.out.println(adq.pollFirst());
+        System.out.println("pollFirst(): "+adq);
+
+        System.out.println(adq.pollLast());
+        System.out.println("pollLast(): "+adq);
     }
 }
